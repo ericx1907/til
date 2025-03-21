6bfc14b0694d3031b63e0be1f0bccf05173cc05f
chengx <chengx@morgan-aps.com>
2025-03-21 15:56:52 -0500

    inital commit of count.sh this utility scirpt checks number of entries in each subdirectory and compare it to the number of entries in README.md

---
+++ utility/count.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+find . -maxdepth 1 -mindepth 1 \
+\( -path "./utility" -o -path "./docs" -o -path "./.git" -o -path "./venv" \) -prune \
+-o -type d -print0 | \
+while read -d '' dir ; do
+  num_in_dir=$(find $dir -name "*.md" | wc -l)
+  #printf "%5d markdown files in directory %s\n" "$num" "$dir"
+  dir_name=$(echo "$dir" | cut -d/ -f2)
+  num_in_readme="$(rg "]\("$dir_name"/" README.md -c)"
+  if [[ $num_in_dir -gt $num_in_readme ]]; then
+    printf "entries in %s is greater than entries in readme.md!\n" $dir
+  elif [[ $num_in_dir -lt $num_in_readme ]]; then
+    printf "entries in %s is less than entries in readme.md!\n" $dir
+  fi
+done
+  
